4d0318276e2a85a78205af17652603389304a54a
xubo <sunicdavy@sina.com>
2015-06-02 17:31:06 +0800

    init

---
+++ Main.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace LiteMate
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMain = new System.Windows.Forms.ListBox();
+            this.btnClone = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnSetPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstMain
+            // 
+            this.lstMain.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lstMain.FormattingEnabled = true;
+            this.lstMain.ItemHeight = 31;
+            this.lstMain.Location = new System.Drawing.Point(12, 8);
+            this.lstMain.Name = "lstMain";
+            this.lstMain.Size = new System.Drawing.Size(170, 252);
+            this.lstMain.TabIndex = 0;
+            this.lstMain.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstMain_MouseDoubleClick);
+            // 
+            // btnClone
+            // 
+            this.btnClone.Location = new System.Drawing.Point(188, 107);
+            this.btnClone.Name = "btnClone";
+            this.btnClone.Size = new System.Drawing.Size(83, 39);
+            this.btnClone.TabIndex = 2;
+            this.btnClone.Text = "克隆";
+            this.btnClone.UseVisualStyleBackColor = true;
+            this.btnClone.Click += new System.EventHandler(this.btnClone_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(188, 197);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 39);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(188, 8);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(83, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(188, 152);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(83, 39);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "改名";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // btnSetPath
+            // 
+            this.btnSetPath.Location = new System.Drawing.Point(188, 53);
+            this.btnSetPath.Name = "btnSetPath";
+            this.btnSetPath.Size = new System.Drawing.Size(83, 39);
+            this.btnSetPath.TabIndex = 1;
+            this.btnSetPath.Text = "设置路径";
+            this.btnSetPath.UseVisualStyleBackColor = true;
+            this.btnSetPath.Click += new System.EventHandler(this.btnSetPath_Click);
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(285, 268);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSetPath);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClone);
+            this.Controls.Add(this.lstMain);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "LiteMate";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.ListBox lstMain;
+        private System.Windows.Forms.Button btnClone;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnSetPath;
+
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Configuration;
+
+namespace LiteMate
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }        
+
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+
+            foreach (Environment.SpecialFolder hs1 in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                Debug.WriteLine(hs1, Environment.GetFolderPath(hs1));
+            }
+
+
+            var configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LiteMate");
+
+            if ( !Directory.Exists(configPath))
+            {
+                Directory.CreateDirectory(configPath);
+            }
+
+            _configFile = new IniFile(configPath + "\\config.ini");
+
+            RefreshNames();
+
+            LoadConfig();
+        }
+
+        string _fileName;
+        IniFile _configFile;
+        void LoadConfig( )
+        {
+            var dir = Directory.GetCurrentDirectory();
+            _fileName = _configFile.ReadValue("Settings","LiteIDEFileName");
+        }
+
+        void SaveConfig( )
+        {
+            _configFile.WriteValue("Settings", "LiteIDEFileName", _fileName);
+        }
+
+        void RefreshNames( )
+        {
+            var configpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "liteide");
+
+            if (!Directory.Exists(configpath))
+            {                
+                return;
+            }
+
+            var files = Directory.GetFiles(configpath);
+
+            lstMain.Items.Clear();
+
+            foreach (var file in files)
+            {
+                var filename = Path.GetFileNameWithoutExtension(file);
+
+                lstMain.Items.Add(filename);
+            }
+        }
+
+        string GetFullPathName( string name )
+        {
+            var configpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "liteide");
+
+            return string.Format( "{0}\\{1}.ini", configpath , name);
+        }
+
+        bool Rename( string oldName, string newName )
+        {
+            try
+            {
+                File.Move(GetFullPathName(oldName), GetFullPathName(newName));
+            }
+            catch( Exception )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        bool Clone(string oldName, string newName)
+        {
+            try
+            {
+                File.Copy(GetFullPathName(oldName), GetFullPathName(newName), true);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        bool Delete(string name)
+        {
+            try
+            {
+                File.Delete(GetFullPathName(name));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        string GetSelectedName()
+        {
+            return (string)lstMain.SelectedItem;
+        }
+
+
+        private void lstMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            StartWithConfig();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartWithConfig();
+        }
+
+
+        void SetAppFileName( )
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "exe files (*.exe)|*.exe|bat files(*.bat)|*.bat";
+            dialog.FilterIndex = 1;
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+
+            }
+
+            _fileName = dialog.FileName;
+            SaveConfig();
+        }
+
+        void StartWithConfig(  )
+        {
+            if (!File.Exists(_fileName))
+            {
+                SetAppFileName();
+            }
+
+            var selectedName = GetSelectedName();
+            if (selectedName == "")
+                return;
+
+            Clone(selectedName, "liteide");
+
+            Process.Start(_fileName);
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var selectedName = GetSelectedName();
+ 
+            if ( selectedName == "liteide")
+            {
+                MessageBox.Show("默认配置文件不能删除", "无法删除", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+
+            var result = MessageBox.Show("确认删除配置?", "删除配置", MessageBoxButtons.YesNo );
+            if (result != System.Windows.Forms.DialogResult.Yes)
+                return;
+
+            Delete(selectedName);
+        }
+
+
+        private void btnClone_Click(object sender, EventArgs e)
+        {
+            var selectedName = GetSelectedName();
+            var name = Interaction.InputBox("输入名称", "克隆配置", selectedName);
+            if (name == "")
+                return;
+
+            Clone(selectedName, name);
+
+            RefreshNames();
+        }
+
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            var selectedName = GetSelectedName();
+            var name = Interaction.InputBox("输入名称", "修改名字", selectedName);
+            if (name == "")
+                return;
+
+            Rename(selectedName, name);
+
+            RefreshNames();
+        }
+
+        private void btnSetPath_Click(object sender, EventArgs e)
+        {
+            SetAppFileName();
+        }
+
+    }
+}
